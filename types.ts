f4fafa5ce143ad85985ed5d96e4474f79bfc00eb
paguevan-lgtm <paguevan@gmail.com>
2026-02-05 01:24:54 -0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,19 @@
+import { LucideIcon } from 'lucide-react';
+
+export interface ServiceItem {
+  id: number;
+  title: string;
+  description: string;
+  icon: LucideIcon;
+}
+
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export interface DifferentiatorItem {
+  id: number;
+  title: string;
+  description: string;
+}
