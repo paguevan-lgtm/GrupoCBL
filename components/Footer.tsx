4ba9da99dbe0bed906ac08f23736074a0609070b
paguevan-lgtm <paguevan@gmail.com>
2026-02-05 01:57:22 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: React.FC<FooterProps> = ({ onOpenAnalyzer }) => {
             <div className="flex items-center gap-3">
               <div className="w-10 h-10">
                  <img 
-                   src="/CBL.png" 
+                   <img src={`${import.meta.env.BASE_URL}CBL.png`} />
                    alt="Grupo CBL Logo" 
                    className="w-full h-full object-contain"
                  />
